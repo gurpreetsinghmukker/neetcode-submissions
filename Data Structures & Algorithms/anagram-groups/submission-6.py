f7d3a1a63bc2cf36c6e899e87325d78fcc086a45
Gurpreet Singh Mukker <44094960+gurpreetsinghmukker@users.noreply.github.com>
2026-09-05 15:02:15 -0400

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        str_dict = defaultdict(list)
+        for s in strs:
+            char_list = tuple(sorted(s))
+            # if not str_dict.get(char_list, None) == None:
+            str_dict[char_list].append(s)
+            # else:
+            #     str_dict[char_list] = [s]
+        # print(str_dict)
+        return [val for val in str_dict.values()]
