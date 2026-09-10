c043699bc591d6f7e12ca400f976d745e7567e6a
Gurpreet Singh Mukker <44094960+gurpreetsinghmukker@users.noreply.github.com>
2026-09-09 20:26:09 -0400

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if s.isalnum() and len(s) == 1:
+            return True
+            
+        char_list = []
+        for char in s:
+            if (char>='A' and char<='z') or (char>="0" and char<="9"):
+                char_list.append(char.lower())
+        for i, c in enumerate(char_list):
+            if not char_list[i] == char_list[len(char_list)-1-i]:
+                return False
+        return True
+        
